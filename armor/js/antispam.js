2b92c163bb716422dc3fc8b7f79dcadad18c7b01
ᴠ ɪ ɴ ɪ ᴄ ɪ ᴜ s <92491145+vncs69@users.noreply.github.com>
2024-03-09 14:02:07 -0300

    from

--- armor/js/antispam.js
+++
@@ -1,8 +0,0 @@
-const usedCommandRecently = new Set()
-
-const isFiltered = (from) => !!usedCommandRecently.has(from)
-const addFilter = (from) => {
-usedCommandRecently.add(from)
-setTimeout(() => usedCommandRecently.delete(from), 6000) }
-
-module.exports = {isFiltered, addFilter}
